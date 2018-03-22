072ee70bbbe0d60b7b19a476f850683b245d9d17
soundestmammal <robert.checco@gmail.com>
2018-03-21 20:34:45 -0400

    Init React Native Application

---
+++ sunshine/src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Make a component
+const Header = () => {
+	return <Text>This is text</Text>;
+};
+
+// Make the component available to other parts of the app
+export default Header;
