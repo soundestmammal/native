7d26aac0117d2b4718076ad8fe125cbc69761276
soundestmammal <robert.checco@gmail.com>
2018-03-24 22:42:25 -0400

    Wrap up Sunshine

--- sunshine/index.js
+++ sunshine/index.js
@@ -1,10 +1,14 @@
 //Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 //Create a component
 const App = () => (
-		<Header />
+	<View style={{ flex: 1 }}>
+		<Header headerText={'Albums'}/>
+		<AlbumList />
+	</View>
 	);
 
 
